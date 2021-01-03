f94ca7eb56ab70de83a3a232fbb3eafb7faadd6f
Bentley Park <junggil.park@sovoro.kr>
2021-01-03 20:12:58 +0900

    #3: app bar 코드 수정 및 ripple 효과 추가

--- lib/widget/home_app_bar.dart
+++ lib/widget/home_app_bar.dart
@@ -15,59 +15,59 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: SafeArea(
-          top: true,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  // topLeft: Radius.circular(10),
-                  // topRight: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10)),
-              boxShadow: [
-                BoxShadow(
-                  color: provider.colorFromHex('#abdfeb').withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
+    return SafeArea(
+      top: true,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: provider.colorFromHex('#abdfeb').withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
             ),
-            child: Stack(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(left: 10.0),
-                  child: WebsafeSvg.asset(
-                    'images/main_logo.svg',
-                    width: 150,
-                    height: 25,
+          ],
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: WebsafeSvg.asset(
+                  'images/main_logo.svg',
+                  width: 150,
+                  height: 25,
+                ),
+              ),
+            ),
+            Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {
+                  print("onTap called.");
+                },
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: WebsafeSvg.asset(
+                      'images/btn_setting.svg',
+                      width: 45,
+                      height: 45,
+                    ),
                   ),
                 ),
-                GestureDetector(
-                    onTap: () {
-                      print("onTap called.");
-                    },
-                    child: Container(
-                      alignment: Alignment.centerRight,
-                      margin: const EdgeInsets.only(right: 10.0),
-                      child: WebsafeSvg.asset(
-                        'images/btn_setting.svg',
-                        width: 45,
-                        height: 45,
-                      ),
-                    ))
-              ],
+              ),
             ),
-          ),
-        ));
-
-    //   AppBar(
-    //   backgroundColor: Colors.white,
-    //   shadowColor: provider.colorFromHex('#abdfeb'),
-    // );
+          ],
+        ),
+      ),
+    );
   }
 }
