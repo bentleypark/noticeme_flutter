0810f2bd2053d253279d0e7bcc8d526545a7e0bf
Bentley Park <junggil.park@sovoro.kr>
2021-01-23 22:25:23 +0900

    dash line box widget에 lyaoutbuilder 적용

--- lib/widget/dash_line_box.dart
+++ lib/widget/dash_line_box.dart
@@ -13,54 +13,74 @@ class DashLineBox extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: EdgeInsets.only(left: displayWidth(context) * 0.05),
-          child: Dash(
-            direction: Axis.horizontal,
-            dashBorderRadius: 2,
-            dashThickness: 3,
-            length: displayWidth(context) * 0.88,
-            dashColor: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
-          ),
-        ),
-        Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: displayWidth(context) * 0.05),
+        LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Padding(
+              padding: EdgeInsets.only(left: constraints.maxWidth * 0.05),
               child: Dash(
-                direction: Axis.vertical,
+                direction: Axis.horizontal,
                 dashBorderRadius: 2,
                 dashThickness: 3,
-                length: 85,
+                length: constraints.maxWidth * 0.9,
                 dashColor: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
-              ),
-              // ),
+              ));
+        }),
+        Row(
+          children: [
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Padding(
+                  padding: EdgeInsets.only(left: displayWidth(context) * 0.05),
+                  child: Dash(
+                    direction: Axis.vertical,
+                    dashBorderRadius: 2,
+                    dashThickness: 3,
+                    length: 85,
+                    dashColor:
+                        provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
+                  ),
+                );
+              },
             ),
             Spacer(),
-            Text('소모품 리스트가 비었습니다.',style: TextStyle(color: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR), fontSize: 18),),
+            Text(
+              '소모품 리스트가 비었습니다.',
+              style: TextStyle(
+                  color: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
+                  fontSize: 18),
+            ),
             Spacer(),
-            Padding(
-              padding: EdgeInsets.only(right: displayWidth(context) * 0.05),
-              child: Dash(
-                  direction: Axis.vertical,
-                  dashBorderRadius: 2,
-                  dashThickness: 3,
-                  length: 90,
-                  dashColor:
-                      provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR)),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Padding(
+                  padding: EdgeInsets.only(right: displayWidth(context) * 0.05),
+                  child: Dash(
+                    direction: Axis.vertical,
+                    dashBorderRadius: 2,
+                    dashThickness: 3,
+                    length: 85,
+                    dashColor:
+                        provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
+                  ),
+                );
+              },
             ),
           ],
         ),
         Align(
           alignment: Alignment.topCenter,
-          child: Dash(
-            direction: Axis.horizontal,
-            dashBorderRadius: 2,
-            dashThickness: 3,
-            length: displayWidth(context) * 0.88,
-            dashColor: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return Dash(
+                direction: Axis.horizontal,
+                dashBorderRadius: 2,
+                dashThickness: 3,
+                length: constraints.maxWidth * 0.9,
+                dashColor: provider.colorFromHex(HOME_SCREEN_DOTTED_LINE_COLOR),
+              );
+            },
           ),
-        ),
+        )
       ],
     );
   }
